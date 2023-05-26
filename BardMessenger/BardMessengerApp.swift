8116992ee5b7c17b42b73830d0caeed2a832d462
Syed Talha Rizwan <talharizwan013@gmail.com>
2023-05-26 20:17:28 +0500

    Initial Commit

---
+++ BardMessenger/BardMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BardMessengerApp.swift
+//  BardMessenger
+//
+//  Created by Rizwan on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BardMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
